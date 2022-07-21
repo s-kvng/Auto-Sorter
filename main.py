782f2370b4a43cd98a5149292d38a7796481932e
s-kvng <105977478+s-kvng@users.noreply.github.com>
2022-07-21 20:01:03 +0000

    Update 1
    creating a dictionary from input data

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 
 def inputData():
+    rank = dict()
     while True:
-        key = input('Enter Student Name > ')
+        key = input('|NB|: Enter "Done" if you done!! Enter Student Name >  ')
+        
+        if key == 'done' or key == 'Done':
+            break
         
         value = input('Enter Marks > ')
+        value = int(value)
+        
+        rank[key] = value
+        
+    
+    return rank
+
+print(inputData())
+        
+        
+        
