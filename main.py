b1b1d829e22da62a8b29f2b9d700c1854f444405
s-kvng <105977478+s-kvng@users.noreply.github.com>
2022-07-23 11:21:03 +0000

    readFile Update
    readFile function is done buh is in its early stages more updates to follow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import re
 
 def inputData():
     rank = dict()
@@ -8,20 +8,57 @@ def inputData():
         if key == 'done' or key == 'Done':
             break
         
-        value = input('Enter Marks > ')
-        value = int(value)
+        try:
+            value = input('Enter Marks > ')
+            value = int(value)
+        except:
+            continue
+            
         
         rank[key] = value
         
     test = sorted([(v,k) for k,v in rank.items()],reverse=True)
     
+    new_file = input("Name the new file > ")
+    
     for v,k in test:
-        f = open('./views/NewFile.docx', 'a')
+        f = open(f'./views/{new_file}.docx', 'a')
         f.write(f'\n{k} - {v}')
         f.close()
         
 
-inputData()
+
+def readFile():
+    
+    count = dict()
+    file = input("Enter the file name : ")
+    
+    fhand = open(f"./views/{file}")
         
-     
+       # if fhand < 1 :
+          #  fhand = "TestFile.txt"
+    
+    for lines in fhand:
+        kline = re.findall('(\S+\s.+)\s[-:]\s[0-9]+',lines) #find and extract only the names within the file(comes in list form) 
+        vline = re.findall('\S+\s.+\s[-:]\s([0-9]+)',lines)#find and extract only the numbers within the file (comes in list form)
+        
+        for text in kline:
+            for val in vline:
+                val = int(val) #change the numbers which is in string form to int
+                count[text] = val # creating the dictionary
+                
+                
+    test2 = sorted([(v,k) for k,v in count.items()], reverse=True)
+    
+    save_file = input("Enter new file name to save : ")
+    #if save_file < 1:
+            #save_file = "TestFile2.txt" 
+    
+    for v,k in test2:
+        fh = open(f"./views/{save_file}.docx", "a")  
+        fh.write(f"\n{k} - {v}")
+        fh.close()
+        
+
+readFile()
         
